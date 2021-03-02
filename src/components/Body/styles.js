4c32109bf689eb3367a2b8b1c01e50c93ede478b
Marcos Paz <31805148+marcosvspaz@users.noreply.github.com>
2021-03-02 20:25:43 -0300

    Divisão de Layout inicial

--- src/components/Body/styles.js
+++ src/components/Body/styles.js
@@ -6,5 +6,11 @@ border:0;
 width: 100%;
 height: 800px;
 background: pink;
-color: white;
+color: black;
+font-size: 80px;
+text-align: center;
+`
+export const P = styled.p`
+
+
 `
---
+++ src/components/Carrousel/index.js
@@ -0,0 +1,12 @@
+import { Container, P} from './styles'
+
+function Carrousel () {
+ return <Container>
+     Carrousel 
+     <P>W= 100% <br></br> H= 400px<br></br> M-T= 50px</P>
+ 
+ </Container>
+
+}
+
+export default Carrousel
