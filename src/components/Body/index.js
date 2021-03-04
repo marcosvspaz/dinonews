4c6587d3180dbe836615c1be5cb8c1cb250675f6
Marcos Paz <31805148+marcosvspaz@users.noreply.github.com>
2021-03-04 00:14:20 -0300

    Update index.js

--- src/components/Body/index.js
+++ src/components/Body/index.js
@@ -4,15 +4,7 @@ import BoxNews from '../BoxNews'
 function Body () {
  return <Container>
      BODY
-     <P>W= 100% <br></br> H= 800px <br></br> M-T= 30px</P>
-     Global style
-     <P>    margin: 0; <br></br>
-    padding: 0;<br></br>
-    box-sizing: border-box;<br></br>
-    font-size: 14px;<br></br>
-   
-    font-family: Arial, 'Segoe UI', Tahoma, Verdana, sans-serif;
-</P>
+  
 <BoxNews></BoxNews>
 <BoxNews></BoxNews>
 <BoxNews></BoxNews>
